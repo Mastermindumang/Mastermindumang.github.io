6a66037cf0b98ccbaf084e8bb8b69904f85a9e8f
Umang Kochar <umang@swivt.com.np>
2023-03-12 23:37:45 +0530

    asd

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1 ;
+var randomNumber = Math.floor(Math.random() * 6) + 1;
 
 diceImage = "dice" + randomNumber + ".png"; // image number maker
 
-
-var RandomImageSource = "images/" + diceImage ; // images source making
-
-
+var RandomImageSource = "images/" + diceImage; // images source making
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src",RandomImageSource);
+image1.setAttribute("src", RandomImageSource);
 
 // var image2 = document.querySelectorAll("img")[1];
 // image2.setAttribute("src",varRandomImageSource);
 
-var randomNumber2 = Math.floor(Math.random() * 6 ) + 1 ;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 // diceImage2 = "dice" + randomNumber2 + ".png" ; //image number 2 making
 // var RandomImageSource2 = "images/" + diceImage2 ;
 var RandomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",RandomImageSource2)
+image2.setAttribute("src", RandomImageSource2);
 
-if(randomNumber>randomNumber2){
-    document.querySelector("h1").innerHTML= "Player 1 wins";
-}    else if(randomNumber2>randomNumber){
-        document.querySelectorAll("h1").innerHTML = "Player 2 wins!!!!";
-    }
-else{document.querySelector("h1").innerHTML="Draw!!";}
+if (randomNumber > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if (randomNumber2 > randomNumber) {
+  document.querySelectorAll("h1").innerHTML = "Player 2 wins!!!!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!!";
+}
