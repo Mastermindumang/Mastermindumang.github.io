30dfb2f2ca2353adfcd24a62c44afde42327f69f
Umang Kochar <umang@swivt.com.np>
2023-03-12 23:25:46 +0530

    update

--- index.js
+++ index.js
@@ -22,3 +22,10 @@ var RandomImageSource2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",RandomImageSource2)
 
+
+if(randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML= "Player 1 wins"
+}    else if(randomNumber2>randomNumber){
+        document.querySelectorAll("h1").innerHTML = "Player 2 wins!!!!"
+    }
+else{document.querySelector("h1").innerHTML="Draw!!"}
